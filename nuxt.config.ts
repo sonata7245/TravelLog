90d8ced2c1924fd34d829a90b8bb293e59b7d4a0
Sonata7245 <Sonata7245@gmail.com>
2025-08-30 18:48:39 -0700

    :sparkles: setup tailwind / daisy ui / icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
+  modules: ["@nuxt/eslint", "@nuxt/icon"],
+  css: ["../assets/css/main.css"],
   eslint: {
     config: {
       standalone: false,
     },
   },
 
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+
   devServer: {
     host: "0.0.0.0",
     port: 3000, // Or your desired port
   },
-
-  modules: ["@nuxt/eslint"],
 });
