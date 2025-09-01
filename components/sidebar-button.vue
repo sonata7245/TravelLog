1b850aff0cf6a88a671675400344a6afc26907db
Sonata7245 <Sonata7245@gmail.com>
2025-09-01 16:07:33 -0700

    :sparkles: :children_crossing: add collapsible sidebar with buttons fix #21

---
+++ components/sidebar-button.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+const props = defineProps<{
+  label: string;
+  icon: string;
+  href: string;
+  showLabel: boolean;
+}>();
+
+const route = useRoute();
+</script>
+
+<template>
+  <div class="tooltip-right" :data-tip="props.showLabel ? undefined : props.label" :class="{ tooltip: !props.showLabel }">
+    <NuxtLink :to="props.href" :class="{ 'bg-base-200': route.path === props.href, 'justify-center': !props.showLabel, 'justify-start': props.showLabel }" class="flex gap-2 p-2 hover:bg-base-300 hover:cursor-pointer flex-nowrap">
+      <Icon :name="props.icon" size="24" />
+      <Transition name="grow">
+        <span v-if="props.showLabel">{{ props.label }}</span>
+      </Transition>
+    </NuxtLink>
+  </div>
+</template>
+
+<style scoped>
+.grow-enter-active {
+  animation: grow 0.1s;
+}
+.grow-leave-active {
+  animation: grow 0.1s reverse;
+}
+
+@keyframes grow {
+  0% {
+    transform: scale(0);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+</style>
--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,6 +1,32 @@
-<script lang="ts">
+<script setup lang="ts">
+const isSidebarOpen = ref(false);
+
+onMounted(() => {
+  isSidebarOpen.value = localStorage.getItem("isSidebarOpen") === "true";
+});
+
+function toggleSidebar() {
+  isSidebarOpen.value = !isSidebarOpen.value;
+  localStorage.setItem("isSidebarOpen", isSidebarOpen.value.toString());
+}
 </script>
 
 <template>
-  <h1>Dashboard</h1>
+  <div class="flex-1 flex">
+    <div class="bg-base-100 transition-all duration-300" :class="{ 'w-64': isSidebarOpen, 'w-16': !isSidebarOpen }">
+      <div class="hover:cursor-pointer hover:bg-base-200 p-2 flex" :class="{ 'justify-center': !isSidebarOpen, 'justify-end': isSidebarOpen }" @click="toggleSidebar">
+        <Icon v-if="isSidebarOpen" name="tabler:chevron-left" size="32" />
+        <Icon v-else name="tabler:chevron-right" size="32" />
+      </div>
+      <div class="flex flex-col">
+        <SidebarButton :show-label="isSidebarOpen" label="Locations" icon="tabler:map" href="/dashboard" />
+        <SidebarButton :show-label="isSidebarOpen" label="Add Location" icon="tabler:plus" href="/dashboard/add" />
+        <div class="divider" />
+        <SidebarButton :show-label="isSidebarOpen" label="Sign Out" icon="tabler:logout-2" href="/sign-out" />
+      </div>
+    </div>
+    <div class="flex-1">
+      Content!
+    </div>
+  </div>
 </template>
