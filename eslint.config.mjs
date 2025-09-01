b25dff7c14c9677dd7bf48e67d95fe0f7ee9d077
Josh Jones <38821732+sonata7245@users.noreply.github.com>
2025-08-31 19:29:05 -0700

    :sparkles: connected Turso DB and set Schema (#6)
    * :sparkles: connected Turso DB and set Schema
    
    * :sparkles: connected Turso DB and set Schema

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,7 +14,7 @@ export default withNuxt(
       semi: true,
       quotes: "double",
     },
-    ignores: [".pnpm-store/**"],
+    ignores: [".pnpm-store/**", "**/migrations/*"],
   }, {
     rules: {
       "ts/no-redeclare": "off",
---
+++ lib/db/index.ts
@@ -0,0 +1,12 @@
+import { drizzle } from "drizzle-orm/libsql";
+
+import env from "~/lib/env";
+
+import * as schema from "./schema";
+
+const db = drizzle({ connection: {
+  url: env.TURSO_DATABASE_URL,
+  authToken: env.TURSO_AUTH_TOKEN,
+}, casing: "snake_case", schema });
+
+export default db;
