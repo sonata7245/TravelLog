36622a64738732e9de013ef5ba1ad13a736ed2de
Sonata7245 <Sonata7245@gmail.com>
2025-09-01 11:13:00 -0700

    :sparkles: added github auth / dashboard / error page

---
+++ pages/error.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const route = useRoute();
+
+const error = route.query.error || "An uknown error occurred";
+</script>
+
+<template>
+  <div class="card bg-base-300 container mx-auto mt-4 min-h-72 text-center flex flex-col justify-center items-center gap-4">
+    <div role="alert" class="alert alert-error min-w-1/2 text-center">
+      <span>{{ error }}</span>
+    </div>
+    <NuxtLink to="/" class="btn btn-primary">
+      Home
+      <Icon name="tabler:arrow-left" size="32" />
+    </NuxtLink>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -8,9 +8,7 @@
         <p class="py-6">
           Keep track of your travels and adventures. Add locations, photos, and notes to create a digital journal of your journeys.
         </p>
-        <button class="btn btn-secondary">
-          Sign In With Github <Icon name="tabler:brand-github" size="24" />
-        </button>
+        <AuthButton />
       </div>
     </div>
   </div>
