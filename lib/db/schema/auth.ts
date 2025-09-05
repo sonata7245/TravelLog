a846b28705e936e98e7f3c20067787f22801a3b7
Josh Jones <38821732+sonata7245@users.noreply.github.com>
2025-09-04 20:47:17 -0700

    added unique slug generator and location name constraint (#15)

--- lib/db/schema/auth.ts
+++ lib/db/schema/auth.ts
@@ -24,7 +24,7 @@ export const session = sqliteTable("session", {
   updatedAt: integer().notNull(),
   ipAddress: text(),
   userAgent: text(),
-  userId: text()
+  userId: int()
     .notNull()
     .references(() => user.id, { onDelete: "cascade" }),
 });
@@ -33,7 +33,7 @@ export const account = sqliteTable("account", {
   id: int().primaryKey({ autoIncrement: true }),
   accountId: text().notNull(),
   providerId: text().notNull(),
-  userId: text()
+  userId: int()
     .notNull()
     .references(() => user.id, { onDelete: "cascade" }),
   accessToken: text(),
