902e285535bac0114905f77ebf56ab532e7c3e7e
Sonata7245 <Sonata7245@gmail.com>
2025-09-06 22:52:40 -0700

    :sparkles: add selected point indications, flyTo feature

--- components/app/map.client.vue
+++ components/app/map.client.vue
@@ -31,14 +31,28 @@ onMounted(() => {
       :coordinates="[point.long, point.lat]"
     >
       <template #marker>
-        <div class="tooltip toolti-top" :data-tip="point.label">
+        <div
+          class="tooltip toolti-top hover:cursor-pointer"
+          :data-tip="point.name"
+          :class="{
+            'tooltip-open': mapStore.selectedPoint === point,
+          }"
+          @mouseenter="mapStore.selectPointWithoutFlyTo(point)"
+          @mouseleave="mapStore.selectPointWithoutFlyTo(null)"
+        >
           <Icon
             name="tabler:map-pin-filled"
             size="30"
-            class="text-primary"
+            :class="mapStore.selectedPoint === point ? 'text-accent' : 'text-primary'"
           />
         </div>
       </template>
+      <mgl-popup>
+        <h3 class="text-xl">
+          {{ point.name }}
+        </h3>
+        <p>{{ point.description }}</p>
+      </mgl-popup>
     </MglMarker>
   </MglMap>
 </template>
